147982ff196e7e921f567eb1b5aacf508a37beaa
jparkley <parkleydondon@yahoo.com>
2021-11-15 23:24:50 -0800

    Updated CSS

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -2,26 +2,31 @@ import { Link } from 'react-router-dom'
 
 const Nav = ({user, handleSignOut}) => {
     return (
-        <div className="navbar">
-            <div className="navbar-brand">
+        <div className="nav">
+            <div className="nav-brand">
                 <img src="/bag.png" alt="logo" />
                 <h2>Shops</h2>
-            </div>            
-            <div className="collapse navbar-collapse" id="navbarRes">
-                <ul className="navbar-nav">
+            </div>
+            <div className="nav-container">          
+                <ul className="nav-links">
                     <li className="nav-item">
                         <Link to="/" className="nav-link">Home</Link>
                     </li>
-                    <li className="nav-item">                        
+                    <li className="nav-item">
                         <Link to="/profile" className="nav-link">Profile</Link>
                     </li>
-                    <li className="nav-item">                        
+                    <li className="nav-item">
                         <Link to="/shops/3" className="nav-link">Shop</Link>
                     </li>
                 </ul>
-                <div className="navbar-user">
+                <a href="#" className="toggle-menu">
+                    <span className="menu-bar"></span>
+                    <span className="menu-bar"></span>
+                    <span className="menu-bar"></span>
+                </a>
+                <div className="nav-user">
                     Hello, {user.attributes.email}
-                    <button type="buton" onClick={handleSignOut}>Sign Out</button>                    
+                    <button type="button" className="btn" onClick={handleSignOut}>Sign Out</button>
                 </div>
             </div>
         </div>
