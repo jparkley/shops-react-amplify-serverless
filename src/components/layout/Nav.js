b66e7db3ac9c1e3871edf4af8ea5f78e9e779c36
jparkley <parkleydondon@yahoo.com>
2021-11-16 22:56:05 -0800

    Updated toggle menu

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,7 +1,16 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import ToggleMenu from './ToggleMenu'
+import { FaTimes, FaBars } from 'react-icons/fa'
 
 const Nav = ({user, handleSignOut}) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+    let show = isOpen ? 'show' : ''
+
+    const handleToggle = () => {
+        setIsOpen(prev => !prev)
+    }
+
     return (
         <div className="nav">
             <div className="nav-brand">
@@ -9,7 +18,7 @@ const Nav = ({user, handleSignOut}) => {
                 <h2>Shops</h2>
             </div>
             <div className="nav-links">          
-                <ul className="nav-list">
+                <ul className={`nav-list ${show}`}>
                     <li className="nav-item">
                         <Link to="/" className="nav-link">Home</Link>
                     </li>
@@ -23,7 +32,9 @@ const Nav = ({user, handleSignOut}) => {
                         <button type="button" className="btn btn-link nav-link" onClick={handleSignOut}>Sign Out</button>
                     </li>
                 </ul>
-                <ToggleMenu />
+                <a href="#" className="toggle-menu" onClick={handleToggle}>
+                    {isOpen ? <FaTimes />: <FaBars />}
+                </a>
                 <div className="nav-user">
                     <p>{user.attributes.email}</p>
                     <button type="button" className="btn" onClick={handleSignOut}>Sign Out</button>
